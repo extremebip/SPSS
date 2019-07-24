5a1d36dff72273229ea9ac87c5e81eff5740a0a1
extremebip <46025589+extremebip@users.noreply.github.com>
2019-07-24 11:31:21 +0700

    Add timeline view (1)

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/competition', function (){
+    return view('competition');
+});
 
 Auth::routes(['verify' => true]);
 
